d621b74c4ba93fff6b66336ee38d1c57232cfac3
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-11-22 14:25:23 -0300

    Fix identation

--- helpers/decode.ts
+++ helpers/decode.ts
@@ -1,22 +1,21 @@
 import { hashids } from '~/providers/hashids'
 
 export const decode = (id: string): number | null => {
-    try {
-      const ids = hashids.decode(id)
-  
-      if (!ids.length) {
-        return null
-      }
-  
-      const number = Number(ids[0])
-  
-      if (isNaN(number)) {
-        return null
-      }
-  
-      return number
-    } catch (error) {
+  try {
+    const ids = hashids.decode(id)
+
+    if (!ids.length) {
       return null
     }
+
+    const number = Number(ids[0])
+
+    if (isNaN(number)) {
+      return null
+    }
+
+    return number
+  } catch (error) {
+    return null
   }
-  
+}
