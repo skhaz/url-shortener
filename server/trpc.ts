621cc264472957397e2b93be8745a8eeed481353
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-11-22 19:30:38 -0300

    Cosmetic fixes

--- server/trpc.ts
+++ server/trpc.ts
@@ -1,11 +1,11 @@
 import { initTRPC } from '@trpc/server'
 import superjson from 'superjson'
-import { Context } from '~/server/context'
+import type { Context } from '~/server/context'
 
-const t = initTRPC.context<Context>().create({
+const rpc = initTRPC.context<Context>().create({
   transformer: superjson,
 })
 
-export const publicProcedure = t.procedure
-export const router = t.router
-export const middleware = t.middleware
+export const publicProcedure = rpc.procedure
+export const router = rpc.router
+export const middleware = rpc.middleware
