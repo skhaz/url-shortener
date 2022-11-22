5e6211bfae5c7fd494b4f6ef8b064ce58c11f921
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-11-22 13:52:03 -0300

    Add form validation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,62 +1,40 @@
 import type { GetServerSideProps, InferGetServerSidePropsType } from 'next'
-import Image from 'next/image'
+import { useCallback } from 'react'
+import { useForm, SubmitHandler, Resolver } from 'react-hook-form'
+import * as yup from 'yup'
+import { InferType, SchemaOf, AnySchema, ValidationError } from 'yup'
+import { useYupValidationResolver } from '~/hooks/yup'
 import hashids from '~/providers/hashids'
 import prisma from '~/providers/prisma'
 import styles from '~/styles/Home.module.css'
 
+type Input = {
+  url: string
+}
+
+const schema = yup.object({
+  url: yup.string().url().required(),
+})
+
 const Home = () => {
+  const resolver = useYupValidationResolver(schema)
+  const {
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<Input>({ resolver })
+
+  const onSubmit: SubmitHandler<Input> = (data) => console.log(data.url)
+
   return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code className={styles.code}>pages/index.tsx</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
-        </div>
-      </main>
-
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <label htmlFor="input">
+        <p>placeholder</p>
+      </label>
+      <input id="input" type="text" placeholder="placeholder" {...register('url')}></input>
+      <input type="submit" />
+      {errors.url && <p>{errors.url?.message}</p>}
+    </form>
   )
 }
 
