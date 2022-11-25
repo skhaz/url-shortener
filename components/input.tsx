f0255ce9154a02229dde723c39941c8a47249a9c
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-11-25 15:33:28 -0300

    Clean up

--- components/input.tsx
+++ components/input.tsx
@@ -4,11 +4,8 @@ export type InputProps = {
   error?: string
 } & InputHTMLAttributes<HTMLInputElement>
 
-export const Input: FunctionComponent<InputProps> = ({ error, id, ...rest }) => (
+export const Input: FunctionComponent<InputProps> = ({ error, ...rest }) => (
   <div>
-    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={id}>
-      Url
-    </label>
     <input
       className="
         shadow
@@ -41,32 +38,3 @@ export const Input: FunctionComponent<InputProps> = ({ error, id, ...rest }) => 
     )}
   </div>
 )
-
-/*
-<div class="flex justify-center">
-  <div class="mb-3 xl:w-96">
-    <input
-      type="search"
-      class="
-        form-control
-        block
-        w-full
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-      "
-      id="exampleSearch"
-      placeholder="Type query"
-    />
-  </div>
-</div>
-*/
