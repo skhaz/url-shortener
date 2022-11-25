2db3df0fa16f74b14e293d130b6c5505f9c92109
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-11-25 15:24:37 -0300

    Add typing for Input component

--- components/input.tsx
+++ components/input.tsx
@@ -1,13 +1,60 @@
-export const Input = () => (
+import type { FunctionComponent, InputHTMLAttributes } from 'react'
+
+export type InputProps = {
+  error?: string
+} & InputHTMLAttributes<HTMLInputElement>
+
+export const Input: FunctionComponent<InputProps> = ({ error, id, ...rest }) => (
   <div>
-    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="username">
-      Username
+    <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor={id}>
+      Url
     </label>
     <input
-      className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-      id="username"
-      type="text"
-      placeholder="Username"
+      className="
+        shadow
+        appearance-none
+        border
+        rounded
+        w-full
+        py-2
+        px-3
+        text-gray-700
+        leading-tight
+        focus:outline-none
+        focus:shadow-outline"
+      {...rest}
     />
+    {error && (
+      <span className="flex items-center font-medium tracking-wide text-red-500 text-xs mt-1 ml-1">{error}</span>
+    )}
   </div>
 )
+
+/*
+<div class="flex justify-center">
+  <div class="mb-3 xl:w-96">
+    <input
+      type="search"
+      class="
+        form-control
+        block
+        w-full
+        px-3
+        py-1.5
+        text-base
+        font-normal
+        text-gray-700
+        bg-white bg-clip-padding
+        border border-solid border-gray-300
+        rounded
+        transition
+        ease-in-out
+        m-0
+        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+      "
+      id="exampleSearch"
+      placeholder="Type query"
+    />
+  </div>
+</div>
+*/
--- pages/index.tsx
+++ pages/index.tsx
@@ -20,16 +20,20 @@ const Home = () => {
   const onSubmit: SubmitHandler<Form> = (data) => console.log(data.url)
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <Header />
-      <label htmlFor="input">
-        <p>placeholder</p>
-      </label>
-      <input id="input" type="text" placeholder="placeholder" {...register('url')}></input>
-      {errors.url && <p>{errors.url.message}</p>}
-      <Input />
-      <input type="submit" />
-    </form>
+    <div className="flex flex-col w-full max-w-full mx-auto p-4 border border-gray-200 bg-white shadow">
+      <div className="flex flex-col mb-4">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <Header />
+          <label htmlFor="input">
+            <p>placeholder</p>
+          </label>
+          <input id="input" type="text" placeholder="placeholder" {...register('url')}></input>
+          {errors.url && <p>{errors.url.message}</p>}
+          <Input id="url" type="url" placeholder="http://example.com" error="error" />
+          <input type="submit" />
+        </form>
+      </div>
+    </div>
   )
 }
 
