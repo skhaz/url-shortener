9d3b13aed4cf2d3ebbce3017ee1ce5ff399cf104
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-11-27 08:52:33 -0300

    Clean up

--- pages/api/[slug].ts
+++ pages/api/[slug].ts
@@ -1,7 +1,3 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 
-// export default function handler(request: NextApiRequest, response: NextApiResponse) {
-//  response.status(200).json({ name: 'John Doe' })
-//}
-
 export default (request: NextApiRequest, response: NextApiResponse) => response.status(200).json({ name: 'John Doe' })
