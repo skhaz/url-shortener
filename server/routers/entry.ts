996e24aa8a46154056e75268a3aef6b78d82b6d0
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-11-28 10:21:44 -0300

    Cosmetic fixes

--- server/routers/entry.ts
+++ server/routers/entry.ts
@@ -7,7 +7,7 @@ export const entryRouter = router({
   add: publicProcedure
     .input(input)
     .output(output)
-    .mutation(async ({ ctx, input: {url} }) => {
+    .mutation(async ({ ctx, input: { url } }) => {
       console.log('url', JSON.stringify(url))
 
       const { id } = await ctx.prisma.entry.create({ data: { url } })
