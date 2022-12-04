6c86428613308ba66a0fc9bd2fa2d57a35366092
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-12-04 17:20:35 -0300

    Clean up

--- server/context.ts
+++ server/context.ts
@@ -1,11 +1,9 @@
 import type { inferAsyncReturnType } from '@trpc/server'
 import { prisma } from '~/providers/prisma'
-import { redis } from '~/providers/redis'
 
 export const createContext = async () => {
   return {
     prisma,
-    redis,
   }
 }
 
