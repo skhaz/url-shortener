6ac8c81f07165eb9fb461b3db708ee942323fb18
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-12-04 17:00:48 -0300

    Revert

--- server/routers/entry.ts
+++ server/routers/entry.ts
@@ -1,4 +1,3 @@
-import { TOTAL_KEY } from '~/constants/redis'
 import { hashids } from '~/providers/hashids'
 import { schema as input } from '~/schemas/form'
 import { schema as output } from '~/schemas/output'
@@ -12,9 +11,6 @@ export const entryRouter = router({
       const { id } = await ctx.prisma.entry.create({ data: input })
 
       const slug = hashids.encode(id)
-
-      await ctx.redis.connect()
-      await ctx.redis.incr(TOTAL_KEY)
 
       return { slug }
     }),
