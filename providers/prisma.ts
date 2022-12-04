85ae2c7a7505662a1ee2a83036b165e9d8583013
Rodrigo Delduca <rodrigodelduca@gmail.com>
2022-12-04 17:22:16 -0300

    Clean up

--- providers/prisma.ts
+++ providers/prisma.ts
@@ -1,14 +1,3 @@
 import { PrismaClient } from '@prisma/client'
 
-declare let global: { prisma: PrismaClient }
-
-export let prisma: PrismaClient
-
-if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient()
-  }
-  prisma = global.prisma
-}
+export const prisma = new PrismaClient()
